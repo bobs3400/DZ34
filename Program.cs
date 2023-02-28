47a2ca91d09cb8d8954121ece219cb24985d52cd
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-02-28 19:43:43 +1000

    34

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Введите длину массива");
+int size = int.Parse(Console.ReadLine());
+int[] Array(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(100, 999);
+    }
+    return result;
+}
+int FindElements(int[] mass)
+{
+    int a = 0;
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (mass[i] % 2 == 0 ) a++;
+    }
+    return a;
+}
+int[] mass = Array(size);
+Console.WriteLine($"В данном массиве {FindElements(mass)} четных элементов");
